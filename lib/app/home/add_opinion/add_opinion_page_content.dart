f80fb165d76476bd3af6d8ca5651ccd90055beca
Marta Czech <martakarolinacz@gmail.com>
2022-04-11 15:01:23 +0200

    Implement adding places

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var placeName = '';
+  var cakeName = '';
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(hintText: 'Podaj nazwę miejsca'),
+            onChanged: (newValue) {
+              setState(() {
+                placeName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(hintText: 'Podaj nazwę ciastka'),
+            onChanged: (newValue) {
+              setState(() {
+                cakeName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+              onPressed: () {
+                FirebaseFirestore.instance.collection('restaurants').add({
+                  'name': placeName,
+                  'cake': cakeName,
+                  'rating': 3.0,
+                });
+              },
+              child: const Text('Dodaj'))
+        ],
+      ),
     );
   }
 }
