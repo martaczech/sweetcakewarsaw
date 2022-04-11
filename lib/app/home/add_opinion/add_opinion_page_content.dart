b36ca16faedfa0d1cb828bb7c33e1dddf61efb72
Marta Czech <martakarolinacz@gmail.com>
2022-04-11 14:06:08 +0200

    change the contents of folders

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,32 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-class AddAccountPageContent extends StatelessWidget {
-  const AddAccountPageContent({
+class AddOpinionPageContent extends StatelessWidget {
+  const AddOpinionPageContent({
     Key? key,
-    required this.email,
   }) : super(key: key);
-
-  final String? email;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Jesteś zalogogwany jako $email'),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Text('Wyloguj'),
-          ),
-        ],
-      ),
+    return const Center(
+      child: Text('Dwa'),
     );
   }
 }
